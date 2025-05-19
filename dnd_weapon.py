086b282f2bb0a7b72d5cbed99d18ce98954f4825
Harper Nordin <harper.nordin@gmail.com>
2025-05-19 18:12:24 -0500

    pulled out weapon

---
+++ dnd_weapon.py
@@ -0,0 +1,8 @@
+class DnDWeapon():
+    def __init__(self, name, base_stat, damage_die,
+                 damage_die_count=1, damage_type="Piercing"):
+        self.name = name
+        self.base_stat = base_stat
+        self.damage_die = damage_die
+        self.damage_die_count = damage_die_count
+        self.damage_type = damage_type
